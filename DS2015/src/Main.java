f3197aa05e10f99bdfa08172e3b2b10e23b4b2f1
TigerDeng <zdeng@ua.edu>
2015-10-02 15:43:33 -0500

    after class-singly and circular list

--- DS2015/src/Main.java
+++ DS2015/src/Main.java
@@ -1,41 +1,17 @@
-
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ListNode header = new ListNode(); // imaging this is Tiger
-		// insert at first 
-		ListNode sarah = new ListNode(25);
-		sarah.next = header.next;
-		header.next= sarah;
+		SList list = new SList();
+		list.addFirst(25);
+		list.addFirst(0);
+		list.addFirst(1);
+		list.addFirst(2);
+		list.printList();
 		
-
-		ListNode n1 = new ListNode(2);
-		n1.next = header.next;
-		header.next= n1;
-		
-		
-
-		ListNode n2 = new ListNode(1);
-		n2.next = header.next;
-		header.next= n2;
-		
-
-		ListNode n3 = new ListNode(0);
-		n3.next = header.next;
-		header.next= n3;
-		
-		// print the whole list
-		while(header.next != null){
-			System.out.println(header.next.val);
-			header = header.next;
-		}
 		
 		
 		// how to delete the first student
-		if(header.next != null)
-			header.next  = header.next.next;
-		
+//		
 	}
 
 }
---
+++ DS2015/src/SList.java
@@ -0,0 +1,46 @@
+
+public class SList {
+	private ListNode header;
+	private int count = 0;
+	
+	
+	public SList(){
+		header = new ListNode();
+		// only for circular list
+		// header.next = header;
+	}
+	
+	public int getCount(){
+		return count;
+	}
+	
+	public void delFirst(){
+		if(header.next!=null)
+			header.next = header.next.next;
+	}
+	
+	public void addFirst(int x){
+		ListNode tmp = new ListNode(x);
+		tmp.next = header.next;
+		header.next= tmp;
+	}
+	
+	public void printList(){// print the whole list	
+		ListNode runner = header;
+		while(runner.next != null){
+			System.out.print(""+runner.next.val + "\t" );
+			runner = runner.next;
+		}
+		System.out.println();
+	}
+	
+	public int getSum(){
+		int total = 0;
+		ListNode runner = header;
+		while(runner.next != null){
+			total+=runner.next.val;
+			runner = runner.next;
+		}
+		return total;
+	}
+}
